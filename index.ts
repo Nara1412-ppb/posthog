01428c9b046f3fa12c5de80e8dc7e57e9846e074
Narasimha <nandikonda.r@ezetap.com>
2023-02-22 23:44:18 +0530

    added new library to handle buffer

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import { brotliCompressSync, gzipSync } from 'zlib'
 import { Plugin, PluginMeta, ProcessedPluginEvent, RetryError } from '@posthog/plugin-scaffold'
 import { ManagedUpload } from 'aws-sdk/clients/s3'
 import { Blob } from 'aws-sdk/lib/dynamodb/document_client'
+import { Workbook, Worksheet } from 'exceljs'
 
 export type PluginConfig = {
     awsAccessKey: string
@@ -35,22 +36,34 @@ type S3Plugin = Plugin<{
 
 export function convertEventBatchToBuffer(events: ProcessedPluginEvent[], config:PluginConfig): Buffer {
     if(config.uploadFormat === 'xlsx' || config.uploadFormat === 'csv') {
-        let str: any = arrayToCSV(events)
-        return Buffer.from(str, 'binary');
+        let strBuffer: any = arrayToCSV(events)
+        console.log(typeof strBuffer);
+        return strBuffer;
+        // return Buffer.from(str, 'binary');
     } else {
         return Buffer.from(events.map((event) => JSON.stringify(event)).join('\n'), 'utf8')
     }
 }
 
 function arrayToCSV(objArray:any) {
-    const array = typeof objArray !== 'object' ? JSON.parse(objArray) : objArray;
-    let str = `${Object.keys(array[0]).map(value => `"${value}"`).join(",")}` + '\r\n';
+    const workbook = new Workbook();
+    const worksheet = workbook.addWorksheet('My Sheet');
+    const newRows = worksheet.addRows(objArray);
+    worksheet.commit();
+    const buffer = workbook.xlsx.writeBuffer();
+    buffer.then((buffer) => {
+        return buffer;
+    }).catch(e => {
+        console.log(e)
+    })
+    // const array = typeof objArray !== 'object' ? JSON.parse(objArray) : objArray;
+    // let str = `${Object.keys(array[0]).map(value => `"${value}"`).join(",")}` + '\r\n';
 
-    return array.reduce((str:any, next:any) => {
-        str += `${Object.values(next).map(value =>
-            `${(typeof value !== 'object') ? JSON.stringify(value):`"${value}"`}`).join(",")}` + '\r\n';
-        return str;
-       }, str);
+    // return array.reduce((str:any, next:any) => {
+    //     str += `${Object.values(next).map(value =>
+    //         `${(typeof value !== 'object') ? JSON.stringify(value):`"${value}"`}`).join(",")}` + '\r\n';
+    //     return str;
+    //    }, str);
 }
 
 export const setupPlugin: S3Plugin['setupPlugin'] = (meta) => {
@@ -173,51 +186,3 @@ export const sendBatchToS3 = async (events: ProcessedPluginEvent[], meta: Plugin
         })
     })
 }
-
-// export const sendBatchToS3 = async (events: ProcessedPluginEvent[], meta: PluginMeta<S3Plugin>) => {
-//     const { global, config } = meta
-
-//     console.log(`Trying to send batch with size ${events.length} to S3...`)
-
-//     const date = new Date().toISOString()
-//     const [day, time] = date.split('T')
-//     const dayTime = `${day.split('-').join('')}-${time.split(':').join('')}`
-//     const suffix = randomBytes(8).toString('hex')
-
-//     events.forEach((event) => {
-//         const fileName = `${config.prefix || ''}${day}/${dayTime}-${event.event}-${event.person ? event.person : event.distinct_id}.xlsx`;
-//         const params: S3.PutObjectRequest = {
-//             Bucket: config.s3BucketName,
-//             Key: fileName,
-//             Body: convertEventBatchToBuffer(event),
-//         }
-//         if (config.compression === 'gzip') {
-//             params.Key = `${params.Key}.gz`
-//             params.Body = gzipSync(params.Body as Buffer)
-//         }
-
-//         if (config.compression === 'brotli') {
-//             params.Key = `${params.Key}.br`
-//             params.Body = brotliCompressSync(params.Body as Buffer)
-//         }
-
-//         if (config.sse !== 'disabled') {
-//             params.ServerSideEncryption = config.sse
-//         }
-//         if (config.sse === 'aws:kms') {
-//             params.SSEKMSKeyId = config.sseKmsKeyId
-//         }
-
-//         return new Promise<void>((resolve, reject) => {
-//             global.s3.upload(params, (err: Error, _: ManagedUpload.SendData) => {
-//                 if (err) {
-//                     console.error(`Error uploading to S3: ${err.message}`)
-//                     return reject(new RetryError())
-//                 }
-//                 console.log(`Uploaded ${event.event} event with ${event.distinct_id} id to bucket ${config.s3BucketName}`)
-//                 resolve()
-//             })
-//         })
-//     })
-
-// }
